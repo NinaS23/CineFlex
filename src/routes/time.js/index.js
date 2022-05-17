7faad5174eaf77ac57624eb7f3ae56f6e4716775
MariaDuda <mariabevenuto062@gmail.com>
2022-05-17 13:12:14 -0300

    time screen done

--- src/routes/time.js/index.js
+++ src/routes/time.js/index.js
@@ -5,12 +5,12 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 
-export default function Time(){
+export default function Time() {
     const { idFilme } = useParams();
     console.log("filme id: " + idFilme);
-     const [day , setDay] = useState([])
-     const [hora , setHora] = useState([])
-    useEffect(() =>{
+    const [day, setDay] = useState([])
+    const [filme, setFilme] = useState([])
+    useEffect(() => {
         const requisicao = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/movies/${idFilme}/showtimes`);
 
         requisicao.then(resposta => {
@@ -19,9 +19,19 @@ export default function Time(){
         requisicao.catch(err => console.log(err.resposta))
 
 
-    } , [])
+    }, [])
     console.log(day)
-  
+    useEffect(() => {
+        const requisicao = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/movies/${idFilme}/showtimes`);
+
+        requisicao.then(resposta => {
+            setFilme(resposta.data);
+        });
+        requisicao.catch(err => console.log(err.resposta))
+
+
+    }, [])
+    console.log(filme)
 
     return (
         <>
@@ -29,33 +39,44 @@ export default function Time(){
                 <h2>Selecione o Horário</h2>
             </div>
             <div className="horario-filmes">
-                <h1>oi</h1>
+                <h1>Selecione seu horário</h1>
             </div>
             <div className="data">
-            <>
-                        {day.map(dia => {
-                            return (
-                                <>
-                                  
-                                        <h3 key={dia.id} className="info">{dia.weekday} - {dia.date}</h3>
-                                        <div className="flex">
-                                        {dia.showtimes.map((e) => {
-                                            return (
-                                              
-                                                <Link to={`/Sessao/${e.id}`}>
-                                                    <h3 className="orange" key={e.id} >{e.name}</h3>
+                <>
+                    {day.map(dia => {
+                        return (
+                            <>
 
-                                                </Link>
-                                              
-                                     
-                                      )})}
-                                  </div>
-                                  
-                                </>
-                            )
-                        })}
-                    </>
+                                <h3 key={dia.id} className="info">{dia.weekday} - {dia.date}</h3>
+                                <div className="flex">
+                                    {dia.showtimes.map((e) => {
+                                        return (
+
+                                            <Link to={`/Sessao/${e.id}`}>
+                                                <h3 className="orange" key={e.id} >{e.name}</h3>
+
+                                            </Link>
+
+
+                                        )
+                                    })}
+                                </div>
+
+                            </>
+                        )
+                    })}
+                </>
             </div>
+            <footer>
+                <div className="flexionar">
+                    <img className="tamanho-filme" src={filme.posterURL} alt={filme.title} />
+                    <div className="titulo">
+                        {filme.title}
+                    </div>
+                </div>
+            </footer>
+
         </>
+
     )
 }
